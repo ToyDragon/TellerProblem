c99f1805f1f6ba3b1c2f52b7cd508ccb9ecb0b10
Matthew <matthew@matthew-Lenovo-IdeaPad-Y580.(none)>
2014-11-04 18:31:44 -0500

    Made waitForNotify function synchronized to work with Object.wait

--- src/WaitingThread.java
+++ src/WaitingThread.java
@@ -7,9 +7,13 @@ public class WaitingThread extends Thread {
      * Calls wait, dumps the interrupted exception. It isn't relevant for this project, and this makes the code much
      *   prettier to look at.
      */
-    public void waitForNotification(){
+    public synchronized void waitForNotification(){
         try{
             wait();
-        }catch(InterruptedException e){}
+        }catch(InterruptedException e){
+
+        }catch(IllegalMonitorStateException e){
+            e.printStackTrace();
+        }
     }
 }
